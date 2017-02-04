8a7ed8bc25edc19531e18d8f1d6787380f1ecac2
Luke Stanley <lukewgs@gmail.com>
2017-02-04 05:28:11 -0600

    math updated

--- furby_math.py
+++ furby_math.py
@@ -6,7 +6,6 @@ def math(num1, sign, num2):
     elif sign == '*':
         string = str(num1*num2)
     elif sign == '/':
->>>>>>> 5d32cd56e6622064042a2d259f4b61b9e4894527
         string = str(num1/num2)
     else:
         string = "Error!"
