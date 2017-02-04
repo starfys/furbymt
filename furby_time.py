93665dc09e1adccc2897aa5bf40ae3a7c08ed6b0
swwest <swwest97@gmail.com>
2017-02-04 04:54:33 -0600

    Added time variation for furby_time.py

--- furby_time.py
+++ furby_time.py
@@ -1,4 +1,9 @@
 def get_time():
     from time import gmtime, strftime
-    string = strftime("%l:%M")
-    return string
+    string = strftime("%l:%M %p")
+    if (strftime("%l") == 12) and (strftime("%l") == "am") and (strftime("%M") == 00):
+        return "It's high noon!"
+    if (strftime("%M") == 00):
+        return(strftime("%l") + " o'clock")
+    else:
+        return string
