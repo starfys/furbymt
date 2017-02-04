68ae42e00cdc94c0f8b716656f919020c2b3ee21
Luke Stanley <lukewgs@gmail.com>
2017-02-04 04:12:01 -0600

    finished current modules

--- test.py
+++ test.py
@@ -6,28 +6,27 @@ from voiceOut import say
 # Import modules
 #from bee_movie import bee
 #from date import date
-#from fortune import fortune
+#from furby_fortune import fortune
 #from lucky_number import lucky
-#from richard_stallman import stallman
+from furby_richardStallman import stallman
 #from timeFurby import get_time
 #from weather import weather
-from forecast import get_forecast
+#from furby_forecast import get_forecast
+#from furby_time import get_time
 
-say(get_forecast(0))
 
-#print("Time...")
-#say(get_time())
-#say("23:40")
-#print("Weather...")
-#say(weather())
-
-#print("Lucky...")
-#say(lucky("aeuaoeu"))
-#print("Date...")
-#say(date())
-#print("Fortune...")
-#say(fortune())
-#print("Stallman...")
-#say(stallman())
-#print("Bee...")
-#say(bee())
+print("Time...")
+say(get_time())
+say("23:40")
+print("Weather...")
+say(weather())
+print("Lucky...")
+say(lucky("aeuaoeu"))
+print("Date...")
+say(date())
+print("Fortune...")
+say(fortune())
+print("Stallman...")
+say(stallman())
+print("Bee...")
+say(bee())
--- voiceOut.py
+++ voiceOut.py
@@ -3,7 +3,7 @@ import sys
 
 es = espeak.ESpeak()
 es.voice = 'en-us'
-es.speed = 300
+#es.speed = 300
 
 def say( str ):
 	es.say(str)
